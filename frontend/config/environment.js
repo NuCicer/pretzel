1a1b3111fc70c68cdf5d7e5f1db3231a78a8dfb9
DIMITRIUS IONOV <dimitrius@nucicer.com>
2024-02-22 11:03:41 -0800

    changing to https

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -15,7 +15,7 @@ module.exports = function(environment) {
     apiHost: process.env.API_URL || 'http://localhost:5000',
     // apiHost: 'http://sc-15-cdc.it.csiro.au:7000',
     apiNamespace: 'api', // adding to the host for API calls
-    rootURL: '/', // used with Ember local routing
+    rootURL: '/pretzel', // used with Ember local routing
     locationType: 'auto',
     handsOnTableLicenseKey: null,
 
