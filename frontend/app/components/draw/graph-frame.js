d2f79979731e9a7a0ace7d1c250a413acae41725
DIMITRIUS IONOV <dimitrius@nucicer.com>
2024-02-23 09:54:38 -0800

    changing to https

--- frontend/app/components/draw/graph-frame.js
+++ frontend/app/components/draw/graph-frame.js
@@ -227,7 +227,7 @@ export default Component.extend({
         .attr("viewBox", oa.vc.viewBox.bind(oa.vc))
         .attr("preserveAspectRatio", "none" /*"xMinYMin meet"*/)
         .attr("width", "100%" /*graphDim.w*/)
-        .attr("height", 300 /*"auto"*/);
+        .attr("height", graphDim.h /*"auto"*/);
       oa.svgContainer = svgRoot
         .append("svg:g")
         .attr("transform", translateTransform);
